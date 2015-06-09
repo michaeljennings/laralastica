fc775ca056ce07427617a1a980cde65065f72bb9
Michael Jennings <michael.jennings91@gmail.com>
2015-06-10 00:22:18 +0100

    Updated indexes when saved trait so it works

--- src/IndexesWhenSaved.php
+++ src/IndexesWhenSaved.php
@@ -2,12 +2,27 @@
 
 trait IndexesWhenSaved {
 
+    /**
+     * Add the model event listener to index the model when it is saved.
+     */
     protected static function bootIndexesWhenSaved()
     {
-        static::created(function($model)
+        static::saved(function($model)
         {
+            $laralastica = app('Michaeljennings\Laralastica\Contracts\Wrapper');
 
+            $laralastica->add($model->getType(), $model->getKey(), $model->toArray());
         });
+    }
+
+    /**
+     * Get the elasticsearch type.
+     *
+     * @return mixed
+     */
+    public function getType()
+    {
+        return isset($this->type) ? $this->type : $this->table;
     }
 
 }
