42ca7191b9626cf1164f2624275b504fb1835bb8
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2015-06-09 17:06:40 +0100

    Added laravel 5 service provider

---
+++ src/LaralasticaServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Michaeljennings\Laralastica;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaralasticaServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register laralastica to the app.
+	 * 
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
