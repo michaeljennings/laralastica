a9f841121c4240656cf0ff9ea2f5b8c8278bd81a
Michael Jennings <michael.jennings91@gmail.com>
2015-06-09 23:14:42 +0100

    Added common and ranged searches. Added results method.

--- src/Builder.php
+++ src/Builder.php
@@ -1,15 +1,43 @@
 <?php namespace Michaeljennings\Laralastica; 
 
+use Elastica\Client;
+use Elastica\Query\Bool;
+use Elastica\Query\Common;
 use Elastica\Query\Fuzzy;
 use Elastica\Query\Match;
+use Elastica\Query\MatchAll;
 use Elastica\Query\MultiMatch;
+use Elastica\Query\Range;
 
 class Builder {
 
+    /**
+     * An array of queries to be searched.
+     *
+     * @var array
+     */
     protected $query = [];
 
     /**
-     * Find all indexes where the values are matched in the field. The type option
+     * An instance of the elastica client.
+     *
+     * @var Client
+     */
+    protected $client;
+
+    protected $index;
+
+    protected $type;
+
+    public function __construct(Client $client, $index, $type)
+    {
+        $this->client = $client;
+        $this->index = $index;
+        $this->type = $type;
+    }
+
+    /**
+     * Find all documents where the values are matched in the field. The type option
      * allows you to specify the type of match, can be either phrase or phrase_prefix.
      *
      * The phrase match analyzes the text and creates a phrase query out of the
@@ -47,7 +75,7 @@ class Builder {
     }
 
     /**
-     * Find all indexes where the value is matched in the fields. The type option
+     * Find all documents where the value is matched in the fields. The type option
      * allows you to specify the type of match, can be best_fields, most_fields,
      * cross_fields, phrase, phrase_prefix.
      *
@@ -102,7 +130,7 @@ class Builder {
     }
 
     /**
-     * Find all indexes where all possible matching terms are within the specified
+     * Find all documents where all possible matching terms are within the specified
      * fuzziness range. The fuzziness option can be 0, 1, 2 or AUTO, AUTO is
      * recommended.
      *
@@ -126,6 +154,97 @@ class Builder {
         $this->query[] = $fuzzy;
 
         return $this;
+    }
+
+    /**
+     * Finds all documents matching the query but groups common words,
+     * i.e. the, and runs them after the initial query for more efficiency.
+     *
+     * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-common-terms-query.html
+     *
+     * @param string $field
+     * @param string $query
+     * @param float $cutOff
+     * @param int|bool $minimumMatch
+     * @return $this
+     */
+    public function common($field, $query, $cutOff = 0.001, $minimumMatch = false)
+    {
+        $common = new Common($field, $query, $cutOff);
+
+        if ($minimumMatch) {
+            $common->setMinimumShouldMatch($minimumMatch);
+        }
+
+        $this->query[] = $common;
+
+        return $this;
+    }
+
+    /**
+     * A query which matches all documents.
+     *
+     * @return $this
+     */
+    public function matchAll()
+    {
+        $match = new MatchAll();
+
+        $this->query[] = $match;
+
+        return $this;
+    }
+
+    /**
+     * Find all documents in a given range. The range is provided as an array with
+     * at least either a 'lt' or 'lte' key and a 'gt' or 'gte' key.
+     *
+     * 'lt'  stands for less than
+     * 'lte' for less than or equal to
+     * 'gt'  for greater than
+     * 'gte' for greater than or equal to
+     *
+     * @param string $field
+     * @param array $range
+     * @param bool $timeZone
+     * @param bool $format
+     * @return $this
+     */
+    public function range($field, array $range, $timeZone = false, $format = false)
+    {
+        $range = new Range($field, $range);
+
+        if ($timeZone) {
+            $range->setParam('time_zone', $timeZone);
+        }
+
+        if ($format) {
+            $range->setParam('format', $format);
+        }
+
+        $this->query[] = $range;
+
+        return $this;
+    }
+
+    /**
+     * Run the queries on the elastic search type and return the results.
+     *
+     * @return mixed
+     */
+    public function results()
+    {
+        if ( ! empty($this->query)) {
+            $query = new Elastica\Query($this->query);
+            $query->addSort('_score');
+        } else {
+            $query = new Elastica\Query();
+        }
+
+        // Retrieve the result set
+        $resultSet = $this->type->search($query, 1000);
+
+        return $resultSet->getResults();
     }
 
 }
