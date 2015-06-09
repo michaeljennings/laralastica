ccd927768fd4aca9b6136bf8f4c852973af8c75e
Michael Jennings <michael.jennings91@gmail.com>
2015-06-10 00:05:44 +0100

    Updated service provider and added facade

--- src/LaralasticaServiceProvider.php
+++ src/LaralasticaServiceProvider.php
@@ -4,6 +4,18 @@ use Illuminate\Support\ServiceProvider;
 
 class LaralasticaServiceProvider extends ServiceProvider {
 
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
     public function boot()
     {
         $this->publishes([
@@ -20,7 +32,25 @@ class LaralasticaServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		//
+		$this->app->bind('Michaeljennings\Laralastica\Contracts\Wrapper', function()
+        {
+            return new Laralastica(config('laralastica'));
+        });
+
+        $this->app->bind('laralastica', function($app)
+        {
+            return $app['Michaeljennings\Laralastica\Contracts\Wrapper'];
+        });
 	}
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['laralastica', 'Michaeljennings\Laralastica\Contracts\Wrapper'];
+    }
 
 }
