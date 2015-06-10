57389cd869aab82f486afab4ed0e39b3c90727c2
Michael Jennings <michael.jennings91@gmail.com>
2015-06-10 15:57:39 +0100

    Updated default config values

--- config/laralastica.php
+++ config/laralastica.php
@@ -6,10 +6,16 @@ return [
      * The name of the elasticsearch index, must be lower case, alphanumeric, and
      * must not contain any spaces, hyphens or underscores.
      */
-    'index' => 'advancedrentdev',
+    'index' => 'yourindex',
 
+    /**
+     * The host of your elasticsearch cluster.
+     */
     'host' => 'localhost',
 
+    /**
+     * The port your elasticsearch cluster is running on.
+     */
     'port' => 9200,
 
 ];
