1d1740318473371e8e6878ef9f8f00de6d5a58b1
Michael Jennings <michael.jennings91@gmail.com>
2015-06-10 11:17:00 +0100

    Updated IndexesWhenSaved trait to fire an event instead of indexing the document immediately. Added methods to transform data and get the indexable attributes.

--- src/IndexesWhenSaved.php
+++ src/IndexesWhenSaved.php
@@ -1,6 +1,16 @@
 <?php namespace Michaeljennings\Laralastica;
 
+use Illuminate\Database\Eloquent\Model;
+use Michaeljennings\Laralastica\Events\IndexesWhenSaved as IndexesWhenSavedEvent;
+
 trait IndexesWhenSaved {
+
+    /**
+     * An array of columns with the desired data type as the value.
+     *
+     * @var array
+     */
+    protected $dataTypes = ['id' => 'int'];
 
     /**
      * Add the model event listener to index the model when it is saved.
@@ -9,20 +19,57 @@ trait IndexesWhenSaved {
     {
         static::saved(function($model)
         {
-            $laralastica = app('Michaeljennings\Laralastica\Contracts\Wrapper');
-
-            $laralastica->add($model->getType(), $model->getKey(), $model->toArray());
+            static::$dispatcher->fire(new IndexesWhenSavedEvent($model));
         });
     }
 
     /**
-     * Get the elasticsearch type.
+     * Return the attributes to be indexed as an array.
      *
-     * @return mixed
+     * @param Model $model
+     * @return array
      */
-    public function getType()
+    public function getIndexableAttributes(Model $model)
     {
-        return isset($this->type) ? $this->type : $this->table;
+        return $model->getAttributes();
+    }
+
+    /**
+     * Loop through the attributes and type cast them if neccesary.
+     *
+     * @param array $attributes
+     * @return array
+     */
+    public function transformAttributes(array $attributes)
+    {
+        if ( ! empty($this->dataTypes)) {
+            foreach ($attributes as &$attribute) {
+                if (array_key_exists($attribute, $this->dataTypes)) {
+                    switch ($this->dataTypes[$attribute]) {
+                        case "int":
+                            $attribute = (int) $attribute;
+                            break;
+                        case "integer":
+                            $attribute = (int) $attribute;
+                            break;
+                        case "string":
+                            $attribute = (string) $attribute;
+                            break;
+                        case "float":
+                            $attribute = (float) $attribute;
+                            break;
+                        case "bool":
+                            $attribute = (bool) $attribute;
+                            break;
+                        case "boolean":
+                            $attribute = (bool) $attribute;
+                            break;
+                    }
+                }
+            }
+        }
+
+        return $attributes;
     }
 
 }
