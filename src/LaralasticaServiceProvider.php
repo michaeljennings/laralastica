02d6a370315a46017193c00367ac98beb901a58d
Michael Jennings <michael.jennings91@gmail.com>
2016-12-08 14:52:08 +0000

    Update elastica version

--- src/LaralasticaServiceProvider.php
+++ src/LaralasticaServiceProvider.php
@@ -1,56 +1,8 @@
-<?php namespace Michaeljennings\Laralastica;
+<?php
 
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+namespace Michaeljennings\Laralastica;
 
-class LaralasticaServiceProvider extends EventServiceProvider {
+class LaralasticaServiceProvider extends Ser
+{
 
-    /**
-     * The event handler mappings for the package.
-     *
-     * @var array
-     */
-    protected $listeners = [
-        'Michaeljennings\Laralastica\Events\IndexesWhenSaved' => [
-            'Michaeljennings\Laralastica\Handlers\Events\IndexesSavedModel'
-        ],
-        'Michaeljennings\Laralastica\Events\RemovesDocumentWhenDeleted' => [
-            'Michaeljennings\Laralastica\Handlers\Events\RemovesDocumentBelongingToDeletedModel'
-        ]
-    ];
-
-    /**
-     * Bootstrap the application events.
-     *
-     * @param Dispatcher $dispatcher
-     */
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__.'/../config/laralastica.php' => config_path('laralastica.php'),
-        ]);
-
-        $this->mergeConfigFrom(__DIR__.'/../config/laralastica.php', 'laralastica');
-
-        foreach ($this->listeners as $event => $handlers) {
-            foreach ($handlers as $listener) {
-                $this->app['events']->listen($event, $listener);
-            }
-        }
-    }
-
-    /**
-     * Register laralastica to the app.
-     * 
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bind('Michaeljennings\Laralastica\Contracts\Wrapper', function($app)
-        {
-            return new Laralastica(config('laralastica'), $app['request']);
-        });
-
-        $this->app->alias('Michaeljennings\Laralastica\Contracts\Wrapper', 'laralastica');
-    }
 }
