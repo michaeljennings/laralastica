1790fee8ef3e009d7e55f3d76c0e3a3810e21be9
Michael Jennings <michael.jennings91@gmail.com>
2016-12-09 12:32:18 +0000

    Add result test

---
+++ tests/ResultTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Michaeljennings\Laralastica\Tests;
+
+use Michaeljennings\Laralastica\Result;
+
+class ResultTest extends TestCase
+{
+    /** @test */
+    public function it_implements_the_result_interface()
+    {
+        $result = $this->makeResult();
+
+        $this->assertInstanceOf(\Michaeljennings\Laralastica\Contracts\Result::class, $result);
+    }
+    
+    /** @test */
+    public function it_gets_a_attribute_from_the_result()
+    {
+        $result = $this->makeResult();
+
+        $this->assertEquals('bar', $result->get('foo'));
+    }
+
+    /** @test */
+    public function it_gets_an_item_dynamically_from_the_result()
+    {
+        $result = $this->makeResult();
+
+        $this->assertEquals('bar', $result->foo);
+    }
+
+    /** @test */
+    public function it_converts_nested_items_to_a_result_instance()
+    {
+        $result = $this->makeResult();
+
+        $this->assertInstanceOf(\Michaeljennings\Laralastica\Contracts\Result::class, $result->baz);
+    }
+
+    protected function makeResult()
+    {
+        return new Result([
+            'foo' => 'bar',
+            'baz' => [
+                'foo' => 'bar',
+                'baz' => 'qux'
+            ]
+        ]);
+    }
+}
