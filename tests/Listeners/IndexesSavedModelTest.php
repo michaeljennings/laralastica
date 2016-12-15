2c50178c704c3b365a7a0fd0f73421edda2ae70d
Michael Jennings <michael.jennings91@gmail.com>
2016-12-15 16:14:44 +0000

    Add listener tests

---
+++ tests/Listeners/IndexesSavedModelTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Michaeljennings\Laralastica\Tests\Listeners;
+
+use Michaeljennings\Laralastica\Events\IndexesWhenSaved;
+use Michaeljennings\Laralastica\Listeners\IndexesSavedModel;
+use Michaeljennings\Laralastica\Tests\TestCase;
+use Mockery;
+
+class IndexesSavedModelTest extends TestCase
+{
+    /** @test */
+    public function it_handles_adding_a_new_document()
+    {
+        $laralastica = Mockery::mock('Michaeljennings\Laralastica\Laralastica')
+                              ->shouldReceive('add')
+                              ->once()
+                              ->getMock();
+
+        $model = Mockery::mock('Illuminate\Database\Eloquent\Model');
+
+        $model->shouldReceive('getIndexableAttributes')
+              ->once()
+              ->andReturn(['id' => 1, 'name' => 'test']);
+
+        $model->shouldReceive('transformAttributes')
+              ->once()
+              ->andReturn(['id' => 1, 'name' => 'test']);
+
+        $model->shouldReceive('getSearchType')
+              ->once();
+
+        $model->shouldReceive('getSearchKey')
+              ->once();
+
+        $event = new IndexesWhenSaved($model);
+
+        $handler = new IndexesSavedModel($laralastica);
+
+        $handler->handle($event);
+    }
+
+    public function tearDown()
+    {
+        Mockery::close();
+
+        parent::tearDown();
+    }
+}
---
+++ tests/Listeners/RemovesDocumentBelongingToDeletedModelTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Michaeljennings\Laralastica\Tests\Listeners;
+
+use Michaeljennings\Laralastica\Events\RemovesDocumentWhenDeleted;
+use Michaeljennings\Laralastica\Listeners\RemovesDocumentBelongingToDeletedModel;
+use Michaeljennings\Laralastica\Tests\TestCase;
+use Mockery;
+
+class RemovesDocumentBelongingToDeletedModelTest extends TestCase
+{
+    /** @test */
+    public function it_handles_deleting_a_document()
+    {
+        $laralastica = Mockery::mock('Michaeljennings\Laralastica\Laralastica')
+                              ->shouldReceive('delete')
+                              ->once()
+                              ->getMock();
+
+        $model = Mockery::mock('Illuminate\Database\Eloquent\Model');
+
+        $model->shouldReceive('getSearchType')
+              ->once();
+
+        $model->shouldReceive('getSearchKey')
+              ->once();
+
+        $event = new RemovesDocumentWhenDeleted($model);
+
+        $handler = new RemovesDocumentBelongingToDeletedModel($laralastica);
+
+        $handler->handle($event);
+    }
+}
