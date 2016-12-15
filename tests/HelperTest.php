48eef40f4df185caa3339a668a2b7697b5e09336
Michael Jennings <michael.jennings91@gmail.com>
2016-12-15 16:21:35 +0000

    Add helper test

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -1,15 +1,49 @@
 <?php
 
-namespace Michaeljennings\Laralastica\Tests;
+namespace {
 
-use Michaeljennings\Laralastica\Laralastica;
-use Michaeljennings\Laralastica\LaralasticaServiceProvider;
+    use Elastica\Connection;
+    use Illuminate\Http\Request;
+    use Michaeljennings\Laralastica\ClientManager;
+    use Michaeljennings\Laralastica\Laralastica;
 
-class HelperTest extends TestCase
-{
-    /** @test */
-    public function assert_helper_returns_laralastica_instance()
+    function app()
     {
+        $request = new Request(['page' => 1]);
 
+        return new Laralastica(new ClientManager([
+            'driver' => 'null',
+            'index' => 'testindex',
+            'drivers' => [
+                'elastica' => [
+                    'host' => getenv('ES_HOST') ?: Connection::DEFAULT_HOST,
+                    'port' => getenv('ES_PORT') ?: Connection::DEFAULT_PORT,
+                    'size' => 10,
+                ]
+            ]
+        ]), $request);
+    }
+}
+
+namespace Michaeljennings\Laralastica\Tests {
+
+    use Michaeljennings\Laralastica\Contracts\ResultCollection;
+    use Michaeljennings\Laralastica\Laralastica;
+
+    class HelperTest extends TestCase
+    {
+        /** @test */
+        public function assert_helper_returns_laralastica_instance()
+        {
+            $this->assertInstanceOf(Laralastica::class, laralastica());
+        }
+        
+        /** @test */
+        public function it_searches_if_parameters_are_passed_to_laralastica_helper()
+        {
+            $this->assertInstanceOf(ResultCollection::class, laralastica('test', function($query) {
+                $query->matchAll();
+            }));
+        }
     }
 }
