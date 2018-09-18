9eaf07d9c049c2167e22d84567f37414270ba05a
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2018-09-18 16:24:17 +0100

    Add ability to chunk reindex command

--- src/Commands/ReIndexLaralastica.php
+++ src/Commands/ReIndexLaralastica.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Michaeljennings\Laralastica\Events\IndexesWhenSaved;
 use Michaeljennings\Laralastica\Exceptions\IndexableModelNotSetException;
 use Michaeljennings\Laralastica\Jobs\IndexModels;
+use Michaeljennings\Laralastica\Jobs\QueueIndexingModels;
 use Michaeljennings\Laralastica\SearchSoftDeletes;
 
 class ReIndexLaralastica extends Command
@@ -17,7 +18,7 @@ class ReIndexLaralastica extends Command
      *
      * @var string
      */
-    protected $signature = 'laralastica:index {index?} {--queue}';
+    protected $signature = 'laralastica:index {index?} {--queue} {--chunk=}';
 
     /**
      * The console command description.
@@ -57,15 +58,19 @@ class ReIndexLaralastica extends Command
      * Execute the console command.
      *
      * @return mixed
+     * @throws IndexableModelNotSetException
      */
     public function handle()
     {
         $models = $this->getIndexableModels($this->argument('index'));
+        $chunks = $this->option('chunk') ?: 500;
 
-        if ($this->option('queue')) {
-            $this->indexViaQueue($models);
-        } else {
-            $this->index($models);
+        foreach ($models as $key => $model) {
+            if ($this->option('queue')) {
+                $this->indexViaQueue($key, $model, $chunks);
+            } else {
+                $this->index($key, $model, $chunks);
+            }
         }
 
         $this->info("\n\nThe re-indexing has been completed successfully\n");
@@ -74,45 +79,56 @@ class ReIndexLaralastica extends Command
     /**
      * Re-index all of the provided models.
      *
-     * @param $models
+     * @param string $key
+     * @param Model  $model
+     * @param int    $chunks
      */
-    protected function index($models)
+    protected function index($key, Model $model, $chunks)
     {
-        foreach ($models as $key => $model) {
-            $this->info("\n\nRe-indexing " . $key . "\n");
+        $this->info("\n\nRe-indexing " . $key . "\n");
 
-            if (in_array(SearchSoftDeletes::class, class_uses($model))) {
-                $toBeIndexed = $model->withTrashed()->get();
-            } else {
-                $toBeIndexed = $model->all();
-            }
+        $toBeIndexed = $this->toBeIndexed($model);
 
-            $bar = $this->output->createProgressBar(count($toBeIndexed));
+        $bar = $this->output->createProgressBar(count($toBeIndexed));
 
-            foreach ($toBeIndexed as $indexable) {
-                $this->reIndex($indexable);
+        foreach ($toBeIndexed->chunk($chunks) as $chunk) {
+            $this->dispatcher->dispatch(new IndexModels($chunk, $model->getSearchType()));
 
-                $bar->advance();
-            }
-
-            $bar->finish();
+            $bar->advance($chunk->count());
         }
+
+        $bar->finish();
     }
 
     /**
      * Index the provided models via the queue.
      *
-     * @param $models
+     * @param string $key
+     * @param Model  $model
+     * @param int    $chunks
      */
-    protected function indexViaQueue($models)
+    protected function indexViaQueue($key, Model $model, $chunks)
     {
-        foreach ($models as $key => $model) {
-            $this->info("\n\nQueuing " . $key . "\n");
+        $this->info("\n\nQueuing " . $key . "\n");
 
-            $model->chunk(1000, function($indexable) {
-                $this->dispatcher->dispatch(new IndexModels($indexable));
-            });
+        $model->chunk($chunks, function ($indexable) use ($model) {
+            $this->dispatcher->dispatch(new QueueIndexingModels($indexable, $model->getSearchType()));
+        });
+    }
+
+    /**
+     * Get the records to be indexed.
+     *
+     * @param Model $model
+     * @return \Illuminate\Database\Eloquent\Collection|Model[]
+     */
+    protected function toBeIndexed(Model $model)
+    {
+        if (in_array(SearchSoftDeletes::class, class_uses($model))) {
+            return $model->withTrashed()->get();
         }
+
+        return $model->all();
     }
 
     /**
