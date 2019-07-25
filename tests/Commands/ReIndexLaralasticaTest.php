8b8fa1f212a001cb343ec549a6ff1dba0d32176c
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2019-07-25 11:15:57 +0100

    Add reindexing test

---
+++ tests/Commands/ReIndexLaralasticaTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Michaeljennings\Laralastica\Tests\Commands;
+
+use Michaeljennings\Laralastica\Tests\Fixtures\TestModel;
+use Michaeljennings\Laralastica\Tests\TestCase;
+
+class ReIndexLaralasticaTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_reindexes_the_model()
+    {
+        // Set the indexable model
+        config()->set('laralastica.indexable', [
+            'test' => TestModel::class,
+            'another_test' => TestModel::class,
+        ]);
+
+        // Create some models
+        factory(TestModel::class, 3)->create();
+
+        $this->artisan('laralastica:index')
+             ->expectsOutput("\n\nRe-indexing test\n")
+             ->expectsOutput("\n\nRe-indexing another_test\n")
+             ->expectsOutput("\n\nThe re-indexing has been completed successfully\n");
+    }
+
+    /**
+     * @test
+     */
+    public function it_reindexes_a_specific_index()
+    {
+        // Set the indexable model
+        config()->set('laralastica.indexable', [
+            'test' => TestModel::class,
+            'another_test' => TestModel::class,
+        ]);
+
+        // Create some models
+        factory(TestModel::class, 3)->create();
+
+        $this->artisan('laralastica:index test')
+             ->expectsOutput("\n\nRe-indexing test\n")
+             ->expectsOutput("\n\nThe re-indexing has been completed successfully\n");
+    }
+
+    /**
+     * @test
+     */
+    public function it_queues_the_reindexing()
+    {
+        // Set the indexable model
+        config()->set('laralastica.indexable', [
+            'test' => TestModel::class,
+            'another_test' => TestModel::class,
+        ]);
+
+        // Create some models
+        factory(TestModel::class, 3)->create();
+
+        $this->artisan('laralastica:index --queue')
+             ->expectsOutput("\n\nQueuing test\n")
+             ->expectsOutput("\n\nQueuing another_test\n")
+             ->expectsOutput("\n\nThe re-indexing has been completed successfully\n");
+    }
+
+    /**
+     * @test
+     */
+    public function it_changes_the_chunking()
+    {
+        // Set the indexable model
+        config()->set('laralastica.indexable', [
+            'test' => TestModel::class,
+            'another_test' => TestModel::class,
+        ]);
+
+        // Create some models
+        factory(TestModel::class, 3)->create();
+
+        $this->artisan('laralastica:index --chunk=2')
+             ->expectsOutput("\n\nRe-indexing test\n")
+             ->expectsOutput("\n\nRe-indexing another_test\n")
+             ->expectsOutput("\n\nThe re-indexing has been completed successfully\n");
+    }
+}
